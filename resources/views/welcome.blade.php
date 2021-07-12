5c39e13f1199da72a67d6f522496699b6910d036
HalaAdnan585 <halaadnan369@gmail.com>
2021-07-12 13:00:16 +0300

    added a comment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,3 +2,4 @@
         <button id="loginButton" onclick="window.location='login'" >login</button>
         <button id="registerButton"onclick="window.location='register'">register</button>
    
+<h1>hello</h1>
